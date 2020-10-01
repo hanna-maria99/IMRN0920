3490112c05e9bc8f2a31c114f7ba3d73c10c70ba
hanna-maria99 <67776442+hanna-maria99@users.noreply.github.com>
2020-10-01 09:41:48 +0700

    function

---
+++ Tugas 4 Function/function.js
@@ -0,0 +1,27 @@
+//No. 1 
+function teriak(){
+    console.log("Halo Sanbers!");    
+}
+teriak();
+
+//No. 2
+var num1 = 12
+var num2 = 4
+
+function kalikan(){
+    return num1*num2
+}
+var kali = kalikan();
+console.log(kali);
+
+//No. 3
+var name = "Agus"
+var age = 30
+var address = "Jln. Malioboro, Yogyakarta"
+var hobby = "Gaming"
+
+function introduce(){
+    return(name, age,address,hobby);
+}
+var kenalan = introduce();
+console.log("Nama saya " + name + ", umur saya " + age + ", alamat saya di "+ address + ", dan saya punya hobby yaitu "+ hobby+ "!");
